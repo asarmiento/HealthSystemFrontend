f393e0668f736b3afc295e67bed682f5b1d9c2fa
Dyl1413 <dherreracuadra@gmail.com>
2023-08-03 15:12:31 -0600

    fix
    correccion y agregado de datos

--- src/store/modules/users/actions.js
+++ src/store/modules/users/actions.js
@@ -1,6 +1,14 @@
+import axios from "axios";
 
 export default {
 
+    getUsersAction(context){
+       const users = axios.get("/data-users")
+        localStorage.setItem('users',JSON.stringify(users))
+        context.commit('setUserMutation',{
+            users:users
+        })
+    }
 
 
 }
