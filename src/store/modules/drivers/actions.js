f10192b6dbfb18df7c81bb14428cdec31346dbe9
Derekpro4488 <141046881+Derekpro4488@users.noreply.github.com>
2023-08-03 15:54:22 -0600

    feat
    coneccion de carpetas

--- src/store/modules/drivers/actions.js
+++ src/store/modules/drivers/actions.js
@@ -2,13 +2,23 @@ import axios from "axios";
 
 export default {
 
-    getdriversAction(context){
+    getDriversAction(context){
        const Drivers = axios.get("/data-drivers")
+       
         localStorage.setItem('drivers',JSON.stringify(drivers))
         context.commit('setdriversMutation',{
             drivers:drivers
         })
-    }
-
+    },
+    editDriversAction(context){
+        const Drivers = axios.get("/data-HDriversController")
+        
+         localStorage.setItem('drivers',JSON.stringify(drivers))
+         context.commit('setdriversMutation',{
+             drivers:drivers
+         })
+     }
 
 }
+
+
