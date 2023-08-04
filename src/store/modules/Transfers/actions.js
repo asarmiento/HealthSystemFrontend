f0686c3d8a344a08a6641deee0c9e59ce30dcf33
Derekpro4488 <141046881+Derekpro4488@users.noreply.github.com>
2023-08-04 08:57:09 -0600

    fead
    Creaccion de rutas

--- src/store/modules/Transfers/actions.js
+++ src/store/modules/Transfers/actions.js
@@ -8,7 +8,34 @@ export default {
         context.commit('setTransfersMutation',{
             Patients:Transfers
         })
-    }
-
+    },
+    editTransfersAction(context){
+        const Transfers = axios.get("h-transfers/{id}/edit")
+         localStorage.setItem('Transfers',JSON.stringify(Transfers))
+         context.commit('setTransfersMutation',{
+             Patients:Transfers
+         })
+     },
+     destroyTransfersAction(context){
+        const Transfers = axios.get("h-transfers-delete/{id}")
+         localStorage.setItem('Transfers',JSON.stringify(Transfers))
+         context.commit('setTransfersMutation',{
+             Patients:Transfers
+         })
+     },
+     storeTransfersAction(context){
+        const Transfers = axios.get("h-transfers-store")
+         localStorage.setItem('Transfers',JSON.stringify(Transfers))
+         context.commit('setTransfersMutation',{
+             Patients:Transfers
+         })
+     },
+     updateTransfersAction(context){
+        const Transfers = axios.get("h-transfers-update/{id}")
+         localStorage.setItem('Transfers',JSON.stringify(Transfers))
+         context.commit('setTransfersMutation',{
+             Patients:Transfers
+         })
+     }
 
 }
