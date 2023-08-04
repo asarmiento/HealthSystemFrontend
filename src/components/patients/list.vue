483d790560eee0c7b9d9b062132d8a9f023fad96
Derekpro4488 <141046881+Derekpro4488@users.noreply.github.com>
2023-08-04 11:01:56 -0600

    fix
    correccion de list.vue

--- src/components/patients/list.vue
+++ src/components/patients/list.vue
@@ -15,12 +15,52 @@
 </template>
 
 <script>
-    import DataTable from 'datatables.net-vue3'
-    import DataTablesCore from 'datatables.net';
     export default {
         name: "list",
-        components:{DataTable,DataTablesCore},
+        mounted(){
+          this.table()
+        },
+        methods:{
+            table(){
+                this.$nextTick(() => {
+                    $('#tablaPatients').dataTable({
+                        "order": [[ 0, "desc" ]],
+                        responsive: true,
+                        dom: '<"table-buttons"B>flrt<bottam>ip',
+                        buttons: [
+                            {
+                                text: 'Registrar Pasientes',
+                                action: function ( dt ) {
+                                    document.location = '/registrar-Pasientes'
+                                },
+                                "titleAttr":"Registrar Pasientes",
+                                "className": "btn btn-primary ml-5"
+                            }
+                        ],
+                        "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ],
+                        "language": {
+                            "processing": "Procesando...",
+                            "lengthMenu": "Desplegar _MENU_ líneas por páginas",
+                            "search": "Buscar",
+                            searchPlaceholder: "Buscar aquí",
+                            "zeroRecords": "No se encontraron resultados",
+                            "info": "Mostrando _START_ a _END_ de _TOTAL_ registros",
+                            "infoEmpty": "No records available",
+                            "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+                            "loadingRecords": "Cargando...",
+                            "paginate": {
+                                "first": "Primero",
+                                "last": "Último",
+                                "next": "Siguiente",
+                                "previous": "Anterior"
+                            }},
+                        paginate:[
 
+                        ]
+                    });
+                })
+            }
+        }
     }
 </script>
 
